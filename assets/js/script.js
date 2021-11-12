d6c8a5833a3a3bd748f4d797d3cda728445bb984
Ivan Thiessen <ivant920@gmail.com>
2021-11-11 20:46:49 -0500

    added basic function framework, still needs the exact variables containing the pertinent data, probably needs more complex card entries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
-var resBars = {};
-var flights = {};
+var barsEl = document.querySelector("#resultBars");
+var flightsEl = document.querySelector("#resultFlights");
 
-var displayData = function() {
-    var barCard = $("div").addClass
-}
+var displayIntel = function() {
+    for (var i = 0; i < intel.length; i++) {
+        var barName = intel[i];
+
+        var barEl = document.createElement("div");
+        barEl.classList = "";
+
+        var titleEl = document.createElement("span");
+        titleEl.textContent = barName;
+
+        barEl.appendChild(titleEl);
+
+        barsEl.appendChild(barEl);
+        
+    }
+
+    for (var i = 0; i < intelFlights.length; i++) {
+        var flightName = intelFlights[i];
+
+        var flightEl = document.createElement("div");
+        flightEl.classList = "";
+
+        var titleFlEl = document.createElement("span");
+        titleFlEl.textContent = flightName;
+
+        flightEl.appendChild(titleFlEl);
+
+        flightsEl.appendChild(flightEl);
+    }
+};
