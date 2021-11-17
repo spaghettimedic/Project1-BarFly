e883b3de37f4653b02513ec5f897b014e1f480e6
spaghettimedic <dsteiner247@gmail.com>
2021-11-16 19:34:21 -0500

    successful API fetch with data displaying on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,70 @@
-import { Duffel } from '@duffel/api'
+var lat = "";
+var lon = "";
+var minLat = "";
+var maxLat = "";
+var minLon = "";
+var maxLon = "";
+var openWeatherMapAPIkey = "126fddb2bf227e0327010f96d6495a39";
+var openTripAPIkey = "5ae2e3f221c38a28845f05b6e3685209113606efb34325fcaaa0fedf";
 
-const duffel = new Duffel({
-    token: duffel_test_TCwYLMchbuQPq6TtqLyK_wVVnmKGdgaUW3z-I1XfDPp,
-});
 
-var flightList = document.querySelector(".flightList");
+// this function gets latitude and longitude values that are then altered to get min and max values for each, and those are passed into fetchAmusements to call openTrip API
+var getLatLon = function(userInput) {
+    var getLatLonUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=imperial&appid=" + openWeatherMapAPIkey;
 
-function getFlightResponse() {
-    var requestFlightURL = "https://api.duffel.com/air/offer_requests";
+    fetch(getLatLonUrl)
+    .then(function(response) {
+        $.ajax({
+            url: getLatLonUrl,
+            type: "POST",
+            statusCode: {
+                404: function() {
+                    alert('"' + userInput + '"' + " is not a valid City name. Please try again!");
+                    return;
+                }
+            }
+        });
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        lat = data.coord.lat;
+        lon = data.coord.lon;
+        minLat = lat - 0.25;
+        maxLat = lat + 0.25;
+        minLon = lon - 0.25;
+        maxLon = lon + 0.25;
+        fetchAmusements(minLat, maxLat, minLon, maxLon);
+    })
+};
 
-    $.ajax({
-        url: requestFlightURL,
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("Authorization", "Bearer duffel_test_TCwYLMchbuQPq6TtqLyK_wVVnmKGdgaUW3z-I1XfDPp");
-            xhr.setRequestHeader("Duffel-Version", "beta");
-            xhr.setRequestHeader("Accept", "application/json");
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.setRequestHeader("Accept-Encoding", "gzip");
-        },
-        success: (function(flightResponse) {
-                console.log(flightResponse);
-                getFlightData(flightResponse.json());
-                return flightResponse.json();
-        })
+// results from getLatLon() is passed into this function to make API call - the data from the call is passed into dispalyAmusements()
+var fetchAmusements = function() {
+    var amusementsURL = "https://api.opentripmap.com/0.1/en/places/bbox?lon_min=" + minLon + "&lon_max=" + maxLon + "&lat_min=" + minLat + "&lat_max=" + maxLat + "&kinds=amusements&format=json&limit=10&apikey=" + openTripAPIkey;
+
+    fetch(amusementsURL)
+    .then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        displayAmusements(data);
+        return data;
     });
-
-    // fetch(requestFlightURL)
-    // .then(function(flightResponse) {
-    //     console.log(flightResponse.json());
-    //     return flightResponse.json();
-    // })
-    // .then(function(data) {
-
-    //     for (var i = 0; i < data.length; i++) {
-
-    //         var airline = data[i].offers.slices.segments.operating_carrier.name;
-    //         var departTime = data[i].offers.slices.segments.departing_at;
-    //         var cost = data[i].offers.total_amount;
-    //         var flightListItem = document.createElement("li");
-
-    //         flightListItem.textContent = "Airline: " + airline + "      " + "Depart time: " + departTime + "        " + "Price: " + cost;
-    //         flightList.appendChild(flightListItem);
-    //     }
-    // });
 };
 
-function getFlightData () {
-
+// this function dynamcially creates elements on the page
+var displayAmusements = function(data) {
     for (var i = 0; i < data.length; i++) {
-
-        var airline = data[i].offers.slices.segments.operating_carrier.name;
-        var departTime = data[i].offers.slices.segments.departing_at;
-        var cost = data[i].offers.total_amount;
-        var flightListItem = document.createElement("li");
-
-        flightListItem.textContent = "Airline: " + airline + "      " + "Depart time: " + departTime + "        " + "Price: " + cost;
-        flightList.appendChild(flightListItem);
-    }
-
+        var amusementName = data[i].name;
+        
+        var amusementEl = $("<li>").addClass().text(amusementName);
+        $("#amusementList").append(amusementEl);
+    };
 };
 
+// when search button is clicked, destination field text is passed into getLatLon()
 $("#search").click(function(event) {
     event.preventDefault();
 
-    // get all user inputs and pass them into API call function
-    var origin = $("#originInput").val();
-    var destination = $("#destinationInput").val();
-    var originDate = $("#originDate").val();
-    var destinationDate = $("#destinationDate").val();
-    getFlightResponse(origin, destination, originDate, destinationDate);
+    userInput = $("#destinationInput").val();
+    getLatLon(userInput);
 });
